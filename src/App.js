d951976f9ce3e0a9ec699fc85d4a7190db00edc0
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2019-10-31 18:10:28 +0100

    Trying ThemeProvider (TEST)

--- src/App.js
+++ src/App.js
@@ -2,24 +2,32 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Button } from '@material-ui/core/Button';
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import { green, blue } from '@material-ui/core/colors';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: green,
+    secondary: blue
+  }
+});
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <Button>
+            Ok
+          </Button>
+        </header>
+      </div>
+    </ThemeProvider>
   );
 }
 
