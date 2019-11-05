68593205a3a1b7345d111659ae1f16f5afbb6068
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2019-11-05 15:44:02 +0100

    Night mode :new_moon_with_face:

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -5,44 +5,45 @@ import Headline from 'components/headline/Headline';
 import Home from 'components/home/Home';
 import Header from 'components/header/Header';
 
-// import themeObject from 'theme/Theme';
+import themeObject from 'theme/Theme';
 import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import { CssBaseline, FormControlLabel, Switch } from '@material-ui/core';
 // import Button from '@material-ui/core/Button';
 
 
-const themeObject = createMuiTheme({
-  palette: {
-    primary: {
-      main: '#fff'
-    },
-    secondary: {
-      main: '#FE4225'
-    },
-    type: 'light'
-  },
-  typography: {
-    body1: {
-      fontFamily: ["Palatino Linotype", "Book Antiqua", "Palatino", "serif"].join(',')
-    },
-    button: {
-      textTransform: "capitalize"
-    }
-  }
-});
+// const themeObject = createMuiTheme({
+//   palette: {
+//     primary: {
+//       main: '#fff'
+//     },
+//     secondary: {
+//       main: '#FE4225'
+//     },
+//     type: 'light'
+//   },
+//   typography: {
+//     body1: {
+//       fontFamily: ["Palatino Linotype", "Book Antiqua", "Palatino", "serif"].join(',')
+//     },
+//     button: {
+//       textTransform: "capitalize"
+//     }
+//   }
+// });
 
 const useDarkMode = () => {
   const [theme, setTheme] = useState(themeObject)
 
   const { palette: { type }} = theme;
+  console.info(theme.palette.secondary);
 
   const toggleDarkMode = () => {
     const updatedTheme = {
       ...theme,
       palette: {
         // ...theme.palette,
+        secondary: { main: '#FE4225'},
         type: type === 'light' ? 'dark' : 'light'
-        // type: 'dark'
       }
     }
     setTheme(updatedTheme)
