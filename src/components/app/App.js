ad1545c63e2f2437d228bcf6836a27d64f336730
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2019-11-18 12:14:08 +0100

    Changes from master

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,6 +1,11 @@
 /* eslint-disable no-unused-vars */
 import React, {useState, useReducer} from 'react'
 import {Context, initialState, reducer} from 'stores/store'
+import {Helmet} from 'react-helmet'
+
+// React router
+import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom'
+
 import './App.css'
 import Home from 'components/home/Home'
 import Header from 'components/header/Header'
@@ -32,6 +37,10 @@ const useDarkMode = mode => {
   return [theme, toggleDarkMode]
 }
 
+const Detail = () => {
+  return <h1>Detail</h1>
+}
+
 function App() {
   // Taking stores data
   const [store, dispatch] = useReducer(reducer, initialState)
@@ -59,6 +68,7 @@ function App() {
   })
 
   return (
+<<<<<<< Updated upstream
     <Context.Provider value={{store, dispatch}}>
       {/* <ThemeProvider theme={themeConfig}> */}
       <ThemeProvider theme={testTheme}>
@@ -72,6 +82,40 @@ function App() {
         {/* Footer */}
       </ThemeProvider>
     </Context.Provider>
+=======
+    <Router>
+      <Context.Provider value={{store, dispatch}}>
+        {/* <ThemeProvider theme={themeConfig}> */}
+        <ThemeProvider theme={testTheme}>
+          <Helmet>
+            <title>News App | Today's headlines</title>
+            <meta
+              name='description'
+              content='Headlines News. Your news app. Headlines by country. News by topic'
+            />
+            <link
+              rel='canonical'
+              href='https://newsapp-daniel-belmonte.netlify.com/'
+            />
+          </Helmet>
+          <CssBaseline />
+          {/* <FormControlLabel
+              control={<Switch onClick={toggleDarkMode} aria-label="night mode switch" />}
+            /> */}
+          <Header />
+          <Switch>
+            <Route path='/'>
+              <Home></Home>
+            </Route>
+            <Route path='/headline'>
+              <Detail />
+            </Route>
+          </Switch>
+          <Footer />
+        </ThemeProvider>
+      </Context.Provider>
+    </Router>
+>>>>>>> Stashed changes
   )
 }
 
