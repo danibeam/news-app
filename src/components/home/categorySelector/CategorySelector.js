ec589134d96009f31f16c409bfb13dc5d8534451
Daniel Belmonte <d.belmonte.amoros@accenture.com>
2019-11-20 09:13:02 +0100

    Margin between categories

--- src/components/home/categorySelector/CategorySelector.js
+++ src/components/home/categorySelector/CategorySelector.js
@@ -3,8 +3,16 @@ import React from 'react'
 import categoriesList from './categoriesList'
 
 import {Button} from '@material-ui/core'
+import {makeStyles} from '@material-ui/core/styles'
+
+const useStyles = makeStyles({
+  category: {
+    margin: '5px',
+  },
+})
 
 const CategorySelector = props => {
+  const classes = useStyles()
   return (
     <React.Fragment>
       {categoriesList.map((category, index) => (
@@ -13,6 +21,7 @@ const CategorySelector = props => {
           color={category === props.selected ? 'secondary' : 'default'}
           onClick={() => props.onClick(category)}
           key={index}
+          className={classes.category}
         >
           {category}
         </Button>
